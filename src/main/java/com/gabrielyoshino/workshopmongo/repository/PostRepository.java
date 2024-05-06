33c353aa240a07f17fc8fa3e1f1e71bf2c05d06b
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-05-06 18:29:51 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/gabrielyoshino/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.gabrielyoshino.workshopmongo.repository;
+
+import com.gabrielyoshino.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
