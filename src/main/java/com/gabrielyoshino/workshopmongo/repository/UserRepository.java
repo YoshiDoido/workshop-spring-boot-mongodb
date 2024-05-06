92d79c1d4baa8806a80b2f3cac4243539a97e976
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-05-06 16:31:38 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/gabrielyoshino/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielyoshino.workshopmongo.repository;
+
+import com.gabrielyoshino.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
